67b12e7f85518dcf6c4de79b2b1e6c8308400c4a
Editalthte <jordan.klinefelter+gitlab@protonmail.com>
2020-02-13 13:36:01 -0600

    Add project files.

---
+++ Dependency Chain Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dependency_Chain_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
