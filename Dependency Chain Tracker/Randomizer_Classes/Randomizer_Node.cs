e40fc10316fabfc611082c6cc2739c338426a497
Editalthte <jordan.klinefelter+gitlab@protonmail.com>
2020-05-10 19:26:54 -0500

    Changed type from class to struct, as the object is more or less just a collection a data.  Added a few more data members, and changed all current ones to readonly, as they should be immutable during runtime.

--- Dependency Chain Tracker/Randomizer_Classes/Randomizer_Node.cs
+++ Dependency Chain Tracker/Randomizer_Classes/Randomizer_Node.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -7,20 +8,34 @@ using System.Threading.Tasks;
 
 namespace Dependency_Chain_Tracker
 {
-    public class Randomizer_Node
+    public struct Randomizer_Node
     {
-        public string category { get; set; }    //Item, Location, etc.
-        public string name { get; set; }    //("Boomerang", "Forest_Temple_Entrance", etc.)
+        private readonly string category { get; }    //Item, Location, etc.
+        private readonly string name { get; }    //("Boomerang", "Forest_Temple_Entrance", etc.)
 
-        public string ID { get; set; }  //Used if there are multiple copies of the same named item.  Eg, the Hookshot.
-        public uint totalCopies { get; set; }   //How many total copies of the item are in the run.
+        //Differentiates multiple copies of the same named item.  Eg, the Hookshot.
+        //Of type "string" to allow for non-numeric IDs, should the need arise.
+        private readonly string ID { get; }
+
+        
+        private readonly string key { get; }    //the key is used to quickly cross-reference between dictionary and graph entries
+
+        private readonly uint totalCopies { get; }   //How many total copies of the item are in the run.
                                                 //Eg: Hookshot would be 2, while Strength_Upgrade would be 3
 
         //default constructor
-        public Randomizer_Node()
-        { }
+        public Randomizer_Node(string INIT_CATEGORY, string INIT_NAME, string INIT_ID, uint INIT_TOTAL_COPIES)
+        {
+            category = INIT_CATEGORY;
+            name = INIT_NAME;
+            ID = INIT_ID;
+            totalCopies = INIT_TOTAL_COPIES;
+            key = name + ID;
+        }
 
         
     
     }
+
+    
 }
